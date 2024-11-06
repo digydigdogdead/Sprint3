c8e7c2ba1df9998fbdbebdd50636cd3577f11881
bart <bielski.bart1012@gmail.com>
2024-11-06 10:50:08 +0000

    Adding parsingInput.cs and updating program.cs

---
+++ Sprint3/ParsingInput.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint3
+{
+    internal class ParsingInput
+    {
+        public static void ReadFromUserAndParse()
+        {
+            Console.WriteLine("Enter integer:");
+            int userInt = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter decimal:");
+            decimal userDecimal = decimal.Parse(Console.ReadLine());
+            double convertedDivisionResult = userInt / (double) userDecimal;
+            Console.WriteLine($"Result: {convertedDivisionResult}");
+
+
+        }
+    }
+}
--- Sprint3/Program.cs
+++ Sprint3/Program.cs
@@ -8,6 +8,7 @@
             double num1 = 2.2;
             decimal num2 = 4.9m;
             Console.WriteLine($"Converted {num1} and {num2}, result: {MultiplyInt.MultiplyInts((int)num1, (int)num2)}");
+            ParsingInput.ReadFromUserAndParse();
         }
     }
 }
