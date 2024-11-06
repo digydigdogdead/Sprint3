ba8aa1bece237f25a100c9f13847bbbf4163626b
digydigdogdead <carlyle.laurent@gmail.com>
2024-11-06 10:58:56 +0000

    Added a pokemon class with a catch method and a run method that instantiates a new Pokemon

---
+++ Sprint3/Pokemon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint3
+{
+    internal class Pokemon
+    {
+        public string Name { get; set; }
+
+        public static void Catch()
+        {
+            Console.WriteLine("Congratulations! You caught a Pokémon.");
+        }
+
+        public static void Run()
+        {
+            Pokemon newPokemon = new Pokemon();
+        }
+    }
+}
--- Sprint3/Program.cs
+++ Sprint3/Program.cs
@@ -4,11 +4,16 @@
     {
         static void Main(string[] args)
         {
+            /*
             Task00.Run();
             double num1 = 2.2;
             decimal num2 = 4.9m;
             Console.WriteLine($"Converted {num1} and {num2}, result: {MultiplyInt.MultiplyInts((int)num1, (int)num2)}");
             ParsingInput.ReadFromUserAndParse();
+            */
+
+            Pokemon.Catch();
+    
         }
     }
 }
