837e29c2fda6568ee8a1104df8e73daa4e338695
digydigdogdead <carlyle.laurent@gmail.com>
2024-11-06 10:16:44 +0000

    Added classes with Run functions that declare simple values

---
+++ Sprint3/Task01.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint3
+{
+    internal class Task01
+    {
+        public static void Run()
+        {
+            float taskFloat = 3.80f;
+            string taskString = "h";
+            double taskDouble = 2.5;
+        }
+    }
+}
