e8221f2bbc3bb6482e11f2ce77f7ba3aafa638fd
bart <bielski.bart1012@gmail.com>
2024-11-06 12:11:26 +0000

    Adding NorthcoderStudents.cs and updating Program.cs to ask user for input, creating Json from the string, and using it to create a new instance of NorthcodersStudent

--- Sprint3/Program.cs
+++ Sprint3/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace Sprint3
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Sprint3
+
 {
     internal class Program
     {
@@ -9,8 +13,8 @@
             //double num1 = 2.2;
             //decimal num2 = 4.9m;
             //Console.WriteLine($"Converted {num1} and {num2}, result: {MultiplyInt.MultiplyInts((int)num1, (int)num2)}");
-            ParsingInput.ReadFromUserAndParse();
-            
+            //ParsingInput.ReadFromUserAndParse();
+
 
             //Pokemon.Catch();
 
@@ -27,6 +31,13 @@
 
             //Console.WriteLine(skeledirge.ToString());
             //Console.WriteLine(suicune.ToString());
+
+            //Console.WriteLine("Enter instructions for creating an instance of Northcoders_Student in the format:  @\'{ 'Name': 'Evie Pom', 'YearsCoding': 6,  'IsDebugging': false,'CodingAffiliation' : 'Poms Who Code'}\';");
+            string userInput = Console.ReadLine();
+            Console.WriteLine(userInput);
+            //string jsonString = "{\"Name\":\"EviePom\",\"YearsCoding\":6,\"IsDebugging\":false,\"CodingAffiliation\":\"PomsWhoCode\"}";
+            NorthcodersStudent member = JsonSerializer.Deserialize<NorthcodersStudent>(userInput);
+            Console.WriteLine(member.Name);
 
         }
     }
