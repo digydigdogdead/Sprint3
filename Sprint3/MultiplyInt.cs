104bb0089af22d9f40aab3a5fe0ed2cd7ba30c40
bart <bielski.bart1012@gmail.com>
2024-11-06 10:34:46 +0000

    Added MultiplyInt.cs and called its method in progam.cs

---
+++ Sprint3/MultiplyInt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint3
+{
+    internal class MultiplyInt
+    {
+        public static int MultiplyInts(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
--- Sprint3/Program.cs
+++ Sprint3/Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             Task00.Run();
+            double num1 = 2.2;
+            decimal num2 = 4.9m;
+            Console.WriteLine($"Converted {num1} and {num2}, result: {MultiplyInt.MultiplyInts((int)num1, (int)num2)}");
         }
     }
 }
