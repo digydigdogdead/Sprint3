8424cdcd332274ddecbf05039ff8d4448e441b37
digydigdogdead <carlyle.laurent@gmail.com>
2024-11-06 12:30:22 +0000

    Added a .ToString override to FireTypePokemon class

---
+++ Sprint3/FireTypePokemon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint3
+{
+    class FireTypePokemon : Pokemon
+    {
+        public static void Burn()
+        {
+            Console.WriteLine("You got burned!");
+        }
+
+        public override void Catch()
+        {
+            Console.WriteLine($"You caught {this.Name}! Careful, it's hot!");
+        }
+
+        public override string ToString()
+        {
+            return "This is a fire type pokemon";
+        }
+    }
+}
