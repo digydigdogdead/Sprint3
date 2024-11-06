4d6477c50c5841090b49f5930320cc8782be352e
digydigdogdead <carlyle.laurent@gmail.com>
2024-11-06 11:30:51 +0000

    Expanded division method to allow user to identify if they would prefer the output in a chararray or as a double.

--- Sprint3/ParsingInput.cs
+++ Sprint3/ParsingInput.cs
@@ -15,7 +15,28 @@ namespace Sprint3
             Console.WriteLine("Enter decimal:");
             decimal userDecimal = decimal.Parse(Console.ReadLine());
             double convertedDivisionResult = userInt / (double) userDecimal;
-            Console.WriteLine($"Result: {convertedDivisionResult}");
+
+            // Checks if user wants a Char array:
+            Console.WriteLine("Respond 'True' if you would like the answer in a character array, or 'false' if you wouldn't.");
+            string charArrayQuestionResponse = Console.ReadLine();
+            bool dump;
+            while (!Boolean.TryParse(charArrayQuestionResponse, out dump))
+            {
+                Console.WriteLine("Not a valid answer, try again.");
+                charArrayQuestionResponse = Console.ReadLine();
+            }
+            
+            bool wantsCharArray = Convert.ToBoolean(charArrayQuestionResponse);
+
+            if (wantsCharArray)
+            {
+                char[] resultArray = convertedDivisionResult.ToString().ToCharArray();
+                foreach (char c in resultArray) { Console.WriteLine(c); }
+            }
+            else
+            {
+                Console.WriteLine($"Result: {convertedDivisionResult}");
+            }
 
 
         }
--- Sprint3/Program.cs
+++ Sprint3/Program.cs
@@ -4,29 +4,29 @@
     {
         static void Main(string[] args)
         {
-            /*
+            
             Task00.Run();
-            double num1 = 2.2;
-            decimal num2 = 4.9m;
-            Console.WriteLine($"Converted {num1} and {num2}, result: {MultiplyInt.MultiplyInts((int)num1, (int)num2)}");
+            //double num1 = 2.2;
+            //decimal num2 = 4.9m;
+            //Console.WriteLine($"Converted {num1} and {num2}, result: {MultiplyInt.MultiplyInts((int)num1, (int)num2)}");
             ParsingInput.ReadFromUserAndParse();
-            */
+            
 
-            Pokemon.Catch();
+            //Pokemon.Catch();
 
-            Pokemon clefairy = new Pokemon();
-            Pokemon suicune = new Pokemon();
-            Pokemon skeledirge = clefairy;
+            //Pokemon clefairy = new Pokemon();
+            //Pokemon suicune = new Pokemon();
+            //Pokemon skeledirge = clefairy;
 
-            Console.WriteLine(skeledirge.GetType());
+            //Console.WriteLine(skeledirge.GetType());
 
-            Console.WriteLine(Object.ReferenceEquals(clefairy, skeledirge));
-            Console.WriteLine(Object.ReferenceEquals(clefairy, suicune));
+            //Console.WriteLine(Object.ReferenceEquals(clefairy, skeledirge));
+            //Console.WriteLine(Object.ReferenceEquals(clefairy, suicune));
 
-            skeledirge.Name = "Skeledirge";
+            //skeledirge.Name = "Skeledirge";
 
-            Console.WriteLine(skeledirge.ToString());
-            Console.WriteLine(suicune.ToString());
+            //Console.WriteLine(skeledirge.ToString());
+            //Console.WriteLine(suicune.ToString());
 
         }
     }
